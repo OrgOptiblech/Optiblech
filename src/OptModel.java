b5bd3260fdf74e2d94a51726c1e3cd78df390d6a
HalonBrent <HalonBrent@you-spam.com>
2015-10-30 16:25:48 +0100

    erste Einbeziehung der Klasse OptModel

--- src/OptModel.java
+++ src/OptModel.java
@@ -1,4 +1,24 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
 
+/** enthält alle Formen und die Hauptlogik
+ * 
+ */
 public class OptModel {
-
+	
+	boolean isForm1 = false;
+	
+	/** Haupt-Zeichnen-Funktion */
+	public void zeichnen(Graphics2D g2) {
+		if(isForm1) {
+			g2.setColor(Color.blue);
+			g2.drawRect(10, 10, 100, 100);
+		} else {
+			g2.setColor(Color.red);
+			g2.drawRect(100, 100, 100, 100);
+		}
+	}
+	
+	
+	
 }
--- src/OptView.java
+++ src/OptView.java
@@ -4,6 +4,9 @@ import javax.swing.JLabel;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 /** verantwortliche Klasse für die Darstellung */
 public class OptView extends JFrame {
@@ -12,28 +15,34 @@ public class OptView extends JFrame {
 	JButton bn_test;
 	
 	/** Zeichenflaechenklasse
-	 * muss die Funktion paintComponent überschreiben */
+	 * muss die Funktion paintComponent überschreiben 
+	 * hält einen Zeiger auf die Graphics2D Zeichenflaeche*/
 	class Zeichenflaeche extends JLabel {
+		Graphics2D m_g2;
+		
 		@Override protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			Graphics2D g2 = (Graphics2D)g; //aus Kompatibilitätsgründen
-			zeichnen(g2); //private zeichenfunktion
+			m_g2 = (Graphics2D)g; //aus Kompatibilitätsgründen
+			zeichnen(m_g2); //private zeichenfunktion
 		}
 	}
 	
 	Zeichenflaeche m_zeichenflaeche;
+	
+	OptModel m_model;
 	
 	//Konstruktor
 	public OptView(String title) {
 		super(title);
 		setLayout(null);
 		setBounds(10, 10, 1000, 600);
+		m_model = new OptModel();
 		initWindow();
 	}
 	
 	//Fensterinitialisierung Hilfsfunktion
 	void initWindow() {
-		bn_test = new JButton("Test");
+		bn_test = new JButton("Klick mich");
 		bn_test.setBounds(10, 10, 100, 20);
 		
 		m_zeichenflaeche = new Zeichenflaeche();
@@ -41,12 +50,24 @@ public class OptView extends JFrame {
 		m_zeichenflaeche.setOpaque(true);//undurchsichtig
 		m_zeichenflaeche.setBackground(Color.WHITE);
 		
+		
+		bn_test.addActionListener(new ActionListener()
+		{//anonyme Klasse
+			public void actionPerformed(ActionEvent e)
+			{
+				//Hier die Aktion: z.B. 
+				m_model.isForm1 = true;
+				m_zeichenflaeche.repaint();
+			}
+		});
+		
 		//Dem Fensterbereich hinzufügen
 		getContentPane().add(bn_test);
 		getContentPane().add(m_zeichenflaeche);
 	}
 	
+	/** Wrapper: weiterrleiten an OptModel */
 	void zeichnen(Graphics2D g2) {
-		
+		m_model.zeichnen(g2);
 	}
 }
