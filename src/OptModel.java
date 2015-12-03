806cec4d361c7501db6a0dc452764770fe037248
HalonBrent <HalonBrent@you-spam.com>
2015-12-03 09:14:26 +0100

    testweise ein viertes Rechteck hinzugefügt

--- src/OptModel.java
+++ src/OptModel.java
@@ -7,16 +7,20 @@ import java.util.Vector;
 public class OptModel {
 	
 	public Vector<Rectangle> m_rectsToDraw;
-	public int NO_RECTS_TO_DRAW = 3;
+	public int NO_RECTS_TO_DRAW = 4;
 	
 	public OptModel() {
 		m_rectsToDraw = new Vector<Rectangle>(NO_RECTS_TO_DRAW);
 		Rectangle r1 = new Rectangle(10, 10, 100, 100);
 		Rectangle r2 = new Rectangle(30, 30, 100, 100);
 		Rectangle r3 = new Rectangle(100, 200, 100, 100);
+		Rectangle r4 = new Rectangle(200, 400, 100, 100);
+		
 		m_rectsToDraw.add(r1);
 		m_rectsToDraw.add(r2);
 		m_rectsToDraw.add(r3);
+		m_rectsToDraw.add(r4);
+		
 	}
 	
 	
